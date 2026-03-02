7ba06b2c166cbe724d68047b396b2ebb774c3ce5
Damjan Pavlica <mudroljub@gmail.com>
2026-03-02 07:21:51 +0100

    icons

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -24,7 +24,7 @@ export default function Modal({ region }: Props) {
 
   const getIcon = (fraction: Fraction) => fraction === Fraction.Partisan
     ? <span>★</span>
-    : <span className={styles.germanIcon}>✠</span>
+    : <span className={styles.black}>✠</span>
   const getFlag = (fraction: Fraction) => fraction === Fraction.Partisan
     ? <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Flag_of_Yugoslavia_%281943%E2%80%931946%29.svg/330px-Flag_of_Yugoslavia_%281943%E2%80%931946%29.svg.png" className={styles.flag} />
     : <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Flag_of_Germany_%281935%E2%80%931945%29.svg/330px-Flag_of_Germany_%281935%E2%80%931945%29.svg.png" className={styles.flag} />
@@ -40,7 +40,7 @@ export default function Modal({ region }: Props) {
         </button>
 
         <h2 className={classnames(styles.title, {
-          [styles.germanIcon]: fraction === Fraction.German
+          [styles.black]: fraction === Fraction.German
         })}>
           {getFlag(fraction)} {region.name}
         </h2>
@@ -53,10 +53,10 @@ export default function Modal({ region }: Props) {
           <strong>{fraction}s:</strong>
         </p>
         <ul className={styles.text}>
-          <li>Infantry: {garrison.infantry}</li>
-          {garrison?.artillery! > 0 && <li>Artillery: {garrison.artillery}</li>}
-          {garrison?.tanks! > 0 && <li>Tanks: {garrison.tanks}</li>}
-          {garrison?.aircraft! > 0 && <li>Aircraft: {garrison.aircraft}</li>}
+          <li>Infantry: {garrison.infantry} <img className={styles.icon} src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Civil_Military_Coordination_-_The_Noun_Project.svg" /></li>
+          {garrison?.artillery! > 0 && <li>Artillery: {garrison.artillery} <img className={styles.icon} src="https://upload.wikimedia.org/wikipedia/commons/6/64/Maki1-historic-15.svg" /></li>}
+          {garrison?.tanks! > 0 && <li>Tanks: {garrison.tanks} <img className={styles.icon} src="https://upload.wikimedia.org/wikipedia/commons/f/ff/CH-Zusatztafel-Panzer.svg" /></li>}
+          {garrison?.aircraft! > 0 && <li>Aircraft: {garrison.aircraft} <img className={styles.icon} src="https://upload.wikimedia.org/wikipedia/commons/1/19/Black_aircraft_icon.svg" /></li>}
         </ul>
 
         {attackable && (
