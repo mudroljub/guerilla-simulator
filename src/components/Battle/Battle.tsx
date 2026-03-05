a4de5da728247da0716e9437d719129e611df9b3
Damjan Pavlica <mudroljub@gmail.com>
2026-03-05 22:57:47 +0100

    pomalo

--- src/components/Battle/Battle.tsx
+++ src/components/Battle/Battle.tsx
@@ -1,7 +1,28 @@
+import { v4 as uuidv4 } from 'uuid'
 import { useStore } from "../../store/store"
 import styles from "./Battle.module.scss"
 import Unit from '../Unit/Unit'
 import { Fraction, UnitType } from "../../types/types"
+import { randomInRange, range } from "../../utils/math"
+
+const createUnits = (
+  num: number,
+  fraction: Fraction,
+  unitType: UnitType,
+  xRange: [number, number],
+  yRange = [0, window.innerHeight],
+) =>
+  range(num, () =>
+    <Unit
+      key={uuidv4()}
+      fraction={fraction}
+      unitType={unitType}
+      position={{
+        x: randomInRange(xRange[0], xRange[1]),
+        y: randomInRange(yRange[0], yRange[1]),
+      }}
+    />
+  )
 
 const Battle = () => {
   const { state } = useStore()
@@ -10,13 +31,27 @@ const Battle = () => {
   const current = battleQueue[0]
   const region = regionDict[current]
 
-  const { garrison, attackingForces } = region
-  console.log(garrison, attackingForces)
+  const { attackingForces, garrison } = region
+  
+  const partisans = createUnits(
+    attackingForces!.infantry,
+    Fraction.Partisan,
+    UnitType.infantry,
+    [window.innerWidth * 0.66, window.innerWidth]
+  )
+
+  const germans = createUnits(
+    garrison.infantry,
+    Fraction.German,
+    UnitType.infantry,
+    [0, window.innerWidth * 0.33]
+  )
 
   return (
     <div className={styles.container}>
       <h1>Battle for {region.name}</h1>
-      <Unit fraction={Fraction.German} unitType={UnitType.infantry} position={region.position} />
+      {germans}
+      {partisans}
     </div>
   )
 }
--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,1 +1,6 @@
 export const sample = <T,>(arr: T[]): T => arr[Math.floor(Math.random() * arr.length)]
+
+export const randomInRange = (from: number, to: number) => Math.random() * (to - from) + from
+
+export const range = <T>(length: number, fn: (index: number) => T): T[] =>
+  Array(length).fill(0).map((_, i) => fn(i))
